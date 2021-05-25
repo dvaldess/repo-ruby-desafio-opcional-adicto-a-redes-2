36039244932eb9612d16f5cd8349e45001be50da
Diego <diego.edo.valdes@gmail.com>
2021-05-25 11:07:12 -0400

    actualizado

--- adictos_a_redes2.rb
+++ adictos_a_redes2.rb
@@ -1,6 +1,6 @@
 # Definiendo el metodo solicitado
 def scan_addicts2(array)
-    resultado = [] # Definiendo la variable que guardará el string segun los minutos de uso.
+    resultado = [] # Definiendo el array que guardará el string segun los minutos de uso.
     n = array.count # Aqui se cuentan los minutos que se van a evaluar, en este caso son 9.
     n.times do |i| # Aqui va la iteracion y en el If se genera la condicion
       if array[i] >= 180 # Primera condicion
@@ -13,5 +13,5 @@ def scan_addicts2(array)
     end
     resultado # Es es lo que visualizará el usuario en pantalla
   end
-  print scan_addicts2([179, 89, 181, 30, 90, 10, 200, 0, 500]) # Aqui se indica los valores que recibirá el metodo
-  print "\n"
+print scan_addicts2([179, 89, 181, 30, 90, 10, 200, 0, 500]) # Aqui se indica los valores que recibirá el metodo
+print "\n"
